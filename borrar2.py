31429f2c3ceda0449e1bd6ac43777ff014152c87
Mateo Andres Rubio Hernandez <m.rubioh@sis.virtual.uniandes.edu.co>
2026-09-21 19:18:44 -0500

    archivos de análisis de resultados

---
+++ borrar2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+df = pd.read_csv('analysis/aciertos.csv')
+
+print(df.groupby('estrategia')[['day_match','hour_match','sensor_match','priority_match']].mean().round(3))
+
+cot = df[df.estrategia == 'chain_of_thought']
+print('\nFugas CoT por modelo:')
+print(cot.groupby('modelo').fuga_razonamiento.agg(['sum','count']))
+
+print('\nDistribución de expected_hour:')
+print(df[df.estrategia == 'zero_shot'].expected_hour.value_counts(normalize=True).round(3))
